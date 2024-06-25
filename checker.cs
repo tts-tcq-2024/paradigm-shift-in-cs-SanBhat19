b1beca643dc9079e2b0d8850d4ae9a6d916df091
Sanjay Subrahmanya Bhat <sanq1kor@bosch.com>
2024-06-25 18:57:49 +0530

    Update checker

--- checker.cs
+++ checker.cs
@@ -2,40 +2,71 @@ using System;
 using System.Diagnostics;
 namespace paradigm_shift_csharp
 {
-class Checker
-{
-    static bool batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
-            Console.WriteLine("Temperature is out of range!");
-            return false;
-        } else if(soc < 20 || soc > 80) {
-            Console.WriteLine("State of Charge is out of range!");
-            return false;
-        } else if(chargeRate > 0.8) {
-            Console.WriteLine("Charge Rate is out of range!");
-            return false;
+    class Checker
+    {
+        static bool batteryIsOk(float temperature, float soc, float chargeRate)
+        {
+            bool tempOK = TemperatureIsOk(temperature);
+            bool socOK = SOCIsOk(soc);
+            bool chargeOK = ChargeIsOk(chargeRate);
+            bool check = tempOK && socOK && chargeOK;
+            return check;
         }
-        return true;
-    }
+        static bool TemperatureIsOk(float temperature)
+        {
+            if (temperature < 0 || temperature > 45)
+            {
+                PrintMessage("Temperature is out of range!");
+                return false;
+            }
+            return true;
+        }
+        static bool SOCIsOk(float soc)
+        {
+            if (soc < 20 || soc > 80)
+            {
+                PrintMessage("State of Charge is out of range!");
+                return false;
+            }
+            return true;
+        }
+        static bool ChargeIsOk(float charge)
+        {
+            if (charge > 0.8)
+            {
+                PrintMessage("Charge Rate is out of range!");
+                return false;
+            }
+            return true;
+        }
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void ExpectTrue(bool expression)
+        {
+            if (!expression)
+            {
+                Console.WriteLine("Expected true, but got false");
+                Environment.Exit(1);
+            }
+        }
+        static void ExpectFalse(bool expression)
+        {
+            if (expression)
+            {
+                Console.WriteLine("Expected false, but got true");
+                Environment.Exit(1);
+            }
+        }
+        static int Main()
+        {
+            ExpectTrue(batteryIsOk(25, 70, 0.7f));
+            ExpectFalse(batteryIsOk(50, 85, 0.0f));
+            Console.WriteLine("All ok");
+            return 0;
+        }
 
-    static void ExpectTrue(bool expression) {
-        if(!expression) {
-            Console.WriteLine("Expected true, but got false");
-            Environment.Exit(1);
-        }
     }
-    static void ExpectFalse(bool expression) {
-        if(expression) {
-            Console.WriteLine("Expected false, but got true");
-            Environment.Exit(1);
-        }
-    }
-    static int Main() {
-        ExpectTrue(batteryIsOk(25, 70, 0.7f));
-        ExpectFalse(batteryIsOk(50, 85, 0.0f));
-        Console.WriteLine("All ok");
-        return 0;
-    }
-    
 }
-}
+
