ab152105fa816be051441d70a17bbb060fd6d68d
Sanjay Subrahmanya Bhat <sanq1kor@bosch.com>
2024-06-25 19:09:42 +0530

    Update checker

--- checker.cs
+++ checker.cs
@@ -1,5 +1,8 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
 namespace paradigm_shift_csharp
 {
     class Checker
@@ -16,6 +19,7 @@ namespace paradigm_shift_csharp
         {
             if (temperature < 0 || temperature > 45)
             {
+                Measure(temperature, 45);
                 PrintMessage("Temperature is out of range!");
                 return false;
             }
@@ -25,6 +29,7 @@ namespace paradigm_shift_csharp
         {
             if (soc < 20 || soc > 80)
             {
+                Measure(soc, 80);
                 PrintMessage("State of Charge is out of range!");
                 return false;
             }
@@ -34,10 +39,22 @@ namespace paradigm_shift_csharp
         {
             if (charge > 0.8)
             {
+                Measure(charge, 0.8);
                 PrintMessage("Charge Rate is out of range!");
                 return false;
             }
             return true;
+        }
+        static void Measure(float value, float min, float max)
+        {
+            if(value > max)
+            {
+                PrintMessage("Abnormal - Low")
+            }
+            else
+            {
+                PrintMessage("Abnormal - High")
+            }
         }
         static void PrintMessage(string message)
         {
